9a58aeea1d3b1bc5c3c38a829e18284610d189d1
mit1278 <meetgohel5686@gmail.com>
2024-07-30 13:05:02 +0530

    Create mm.sh

---
+++ mm.sh
@@ -0,0 +1,9 @@
+Codes: https://github.com/dkbroyt/aman/blob/main/aman.sh
+
+git clone https://github.com/dkbroy
+
+cd mm.sh
+
+chmod +x mm.sh && ./mm.sh
+
+common: git add . && git commit -m "feat: initiated the project" && git push origin main
